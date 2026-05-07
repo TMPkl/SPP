50ad0fbe59e3cf41d1a3862f994ef4dccf3d6fac
inf160269 <karol.leszynski@student.put.poznan.pl>
2026-05-07 22:48:19 +0200

    struktury wiadomosci dodane

--- software_esp/SPP/include/messages.h
+++ software_esp/SPP/include/messages.h
@@ -1,12 +1,65 @@
-#ifndef MESSAGES_H
-#define MESSAGES_H
-
 #include <stdint.h>
 #include <string.h>
 
-typedef struct {
-  int id;
-  int value;
-} Message;
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+
+#define CIRCLE_SIZE   3
+
+
+// typy wiadomości 
+typedef enum __attribute__((packed)) {
+    MSG_REQ     = 0x01,
+    MSG_ACK     = 0x02,
+    MSG_REL     = 0x03,
+    MSG_WELCOME = 0x04,
+    MSG_HELLO   = 0x05,
+} msg_type_t;
+
+//te podstawowe dane do wszystkich wiadomości
+typedef struct __attribute__((packed)) {
+    msg_type_t type;
+    uint8_t    from;
+    uint32_t   ts;
+} msg_header_t;
+
+
+// REL(from, ts, participants) 
+typedef struct __attribute__((packed)) {
+    uint8_t participants[CIRCLE_SIZE];
+} payload_rel_t;
+
+// WELCOME(from, ts, deficits, participants)
+typedef struct __attribute__((packed)) {
+    int16_t deficits[3];
+    uint8_t participants[CIRCLE_SIZE];
+} payload_welcome_t;
+
+
+typedef struct __attribute__((packed)) {
+    uint8_t sep;
+    uint8_t alko;      /// ile razy co przyniosłem
+    uint8_t zagrycha; 
+} deficit_t;
+
+// HELLO(from, ts, deficits)
+typedef struct __attribute__((packed)) {
+    int16_t deficits[3];
+    deficit_t my_deficit;
+} payload_hello_t;
+
+
+// razem wszyskot 
+typedef struct __attribute__((packed)) {
+    msg_header_t header;
+    union {
+        payload_rel_t     rel;
+        payload_welcome_t welcome;
+        payload_hello_t   hello;
+        // REQ i ACK - sam nagłówek, brak payloadu
+    } payload;
+} espnow_msg_t;
+
 
 #endif // MESSAGES_H
